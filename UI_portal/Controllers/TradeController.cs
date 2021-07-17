3b75aaf682d4ee4759c497aa0b88ee3217a5c9b3
huynhbavinh <bavinh.congviec@gmail.com>
2021-07-17 14:32:47 +0700

    updating Registeration for free

--- UI_portal/Controllers/TradeController.cs
+++ UI_portal/Controllers/TradeController.cs
@@ -29,6 +29,7 @@ namespace UI_portal.Controllers
 
             return View(postRegistrationModel);
         }
+        //trade với món đồ 
         [HttpPost]
         public async Task<ActionResult> Register(string userId, string postId, string thingId, string comm)
         {
@@ -42,6 +43,31 @@ namespace UI_portal.Controllers
             var registed = await tradeService.getRegistration(model);
             return View("Index");
         }
+        //trade với free
+        [HttpGet]
+        public ActionResult RegisterFree(string postId, string userId)
+        {
+            tradeService = new TradeService();
+            PostRegistration postRegistrationModel = new PostRegistration();
+            postRegistrationModel.post_id = "3f552bf8-0bb7-4d5d-b1e2-179844bcd338"; // "3f552bf8-0bb7-4d5d-b1e2-179844bcd338";
+            postRegistrationModel.user_id = "14551453-4e68-4e40-9aac-fda12a7b11bc"; //"14551453-4e68-4e40-9aac-fda12a7b11bc";
+            ViewData["postRegis"] = postRegistrationModel;
+
+            return View(postRegistrationModel);
+        }
+        [HttpPost]
+        public async Task<ActionResult> RegisterFree(string userId, string postId, string comm)
+        {
+            var model = new PostRegistration();
+            model.post_id = postId;
+            model.user_id = userId;
+            model.description = comm;
+
+            tradeService = new TradeService();
+            var registed = await tradeService.getRegistration(model);
+            return View("Index");
+        }
+
         public async Task<ActionResult> AcceptRegister(string registerPostId)
         {
             tradeService = new TradeService();
